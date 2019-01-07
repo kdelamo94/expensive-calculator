e73a7b8f797c4b21a81ef44900b3e496ca692688
kevin <kdelamo94@gmail.com>
2019-01-07 15:21:59 -0500

    Created the expensive calculator.

---
+++ expensive-calculator.cpp
@@ -0,0 +1,18 @@
+//Expensive Calculator
+//Demonstrates built-in aritmetic operators
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(){
+  cout << "7 + 3 = " << 7 + 3 << endl;
+  cout << "7 - 3 = " << 7 - 3 << endl;
+  cout << "7 * 3 = " << 7 * 3 << endl;
+  cout << "7 / 3 = " << 7 / 3 << endl;
+  cout << "7.0 /3.0 = " << 7.0 / 3.0 << endl;
+  cout << "7 + 3 * 5 = " << 7 + 3 * 5 << endl;
+  cout << "(7 + 3) * 5 = " << (7 + 3) * 5 << endl;
+  return 0;
+}
---
+++ makefile
@@ -0,0 +1,2 @@
+expensive-calculator: expensive-calculator.cpp
+	g++ -o expensive-calculator expensive-calculator.cpp
